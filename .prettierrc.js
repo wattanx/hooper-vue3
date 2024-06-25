bb95712f06db3ead7303584aee1ff5432e5813d1
wattanx <wattan.dev@gmail.com>
2024-06-25 09:24:34 +0900

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  trailingComma: "es5",
+  printWidth: 80,
+};
+
+export default config;
