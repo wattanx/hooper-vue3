d08a9649a3069a449721d31b382deb6039574ada
wattanx <wattan.dev@gmail.com>
2026-01-03 17:45:15 +0900

    chore: fix build settings

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsdown";
+import Vue from "unplugin-vue/rolldown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   external: ["vue"],
-  dts: true,
+  dts: { vue: true },
+  plugins: [Vue({ isProduction: true })],
 });
